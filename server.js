8b39a9d1599a1dbc102f51da18656b99c0998e1b
PrakadAlpha <prakadheshwarancbe@gmail.com>
2020-03-15 11:28:56 +0530

    added models

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const Songs = require('./models/songs');
 const app = express();
 
+mongoose.connect('mongodb://localhost/musics', ({useNewUrlParser: true, useUnifiedTopology: true}))
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+
+
 app.get('/', (req, res) => {
-  res.end('Hello');
+  res.render('index');
+})
+
+app.post('/songs', async (req, res) => {
+  const song = new Songs({
+    notes: req.body.songNotes
+  })
+
+  await song.save();
+
+  res.json(song);
 })
 
 app.listen(5000, () => console.log('Listening on port 5000'));
